3648b5d694eaadea00ad3b89582cb7390e6b8da8
Galina684 <galina684@gmail.com>
2023-11-25 20:52:12 +0300

    Start

---
+++ src/main/java/pro/sky/CourseWork2Galina684/CourseWork2Galina684Application.java
@@ -0,0 +1,13 @@
+package pro.sky.CourseWork2Galina684;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseWork2Galina684Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseWork2Galina684Application.class, args);
+	}
+
+}
