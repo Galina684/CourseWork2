3aceadc28155adb58f2b3c4489a4639824131421
Galina684 <galina684@gmail.com>
2023-11-25 20:53:06 +0300

    Start

---
+++ src/test/java/pro/sky/CourseWork2Galina684/CourseWork2Galina684ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.CourseWork2Galina684;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2Galina684ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
